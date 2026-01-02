a0607054fb04bfafb622476a678c498bfe3a6b02
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-01-02 21:41:16 +0530

    Traversing a LinkedList using while loop

---
+++ LinkedList/4-traversingLinkedList.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+};
+int main(){
+    Node a,b,c,d;
+    a.val = 10;
+    a.next = &b;
+    // Node b;
+    b.val = 20;
+    b.next = &c;
+    // Node c;
+    c.val = 30;
+    c.next = &d;
+    // Node d;
+    d.val = 40;
+    d.next = NULL;
+    // cout<<a.val<<endl;
+    // cout<<b.val<<endl;
+    // cout<<c.val<<endl;
+    // cout<<d.val<<endl;
+    
+
+    Node temp = a;
+    while(true){
+        cout<<temp.val<<" ";
+        if(temp.next==NULL) break;
+        // temp.next = (temp.next)->next;//this will prints the content of 1st node for all nodes except the last node
+        temp = *(temp.next);
+    }
+
+}
