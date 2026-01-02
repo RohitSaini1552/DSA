9bcaf2959d27854143e314e90c5c747a82256291
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-01-02 21:42:25 +0530

    traversing a LinkedList using DoWhile and pointer to Head

---
+++ LinkedList/5-traversingLinkedList2.cpp
@@ -0,0 +1,35 @@
+//write a program to create a linkedlist using a class constructor and traverse the entire list using Do while Loop :
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main() {
+    //forming Nodes 
+    Node a(10), b(20), c(30), d(40);
+
+    // linking
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+
+    // Pointer traversal
+    Node* temp = &a;
+
+    do {
+        cout << temp->val << " ";
+        temp = temp->next;
+    } while (temp != NULL);
+
+    return 0;
+}
