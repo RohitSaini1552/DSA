094c93794aeb56c35257b42a1857e81877e115d6
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-02-17 18:39:27 +0530

    add program to return the height of a binary tree using recursion

---
+++ Tree/8-heightOfTree.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void display(Node* root){
+    if(root == NULL) return;
+    cout<<root->val<<" ";
+    display(root->left);
+    display(root->right);
+}
+int levels(Node* root){ 
+    if(root == NULL) return 0;   
+    int level = 1 + max(levels(root->left) , levels(root->right));
+    return level;
+}
+int main(){
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    Node* e = new Node(50);
+    Node* f = new Node(60);
+    Node* g = new Node(70);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+
+
+    display(a);
+    cout<<endl;
+    cout<<"height of the tree : "<<levels(a);
+}
