b7206f5187fdad84629a38461c1df8c00d5d52d1
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-02-17 18:35:56 +0530

    add program to return the node with max value

---
+++ Tree/5-nodeWithMaxValue.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int maxNodes(Node* root){ 
+    if(root == NULL) return INT_MIN;   
+    return max(root->val , max(maxNodes(root->left) , maxNodes(root->right))) ;
+}
+void display(Node* root){
+    if(root == NULL) return;
+    cout<<root->val<<" ";
+    display(root->left);
+    display(root->right);
+}
+int main(){
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    Node* e = new Node(50);
+    Node* f = new Node(60);
+    Node* g = new Node(70);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+
+
+    display(a);
+    cout<<endl;
+    cout<<"max of all nodes : "<<maxNodes(a);
+}
