8a9dc4cdddcfb6aeabf3edc6873c00a330ce42ed
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-02-17 18:33:48 +0530

    add program to um of value of all the node in a binary tree

---
+++ Tree/3-sumOfAllNodes.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int sumNodes(Node* root){ 
+    if(root == NULL) return 0;   
+    return (root->val) + sumNodes(root->left) + sumNodes(root->right) ;
+}
+void display(Node* root){
+    if(root == NULL) return;
+    cout<<root->val<<" ";
+    display(root->left);
+    display(root->right);
+}
+int main(){
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    Node* e = new Node(50);
+    Node* f = new Node(60);
+    Node* g = new Node(70);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+
+
+    display(a);
+    cout<<endl;
+    cout<<"sum of all nodes : "<<sumNodes(a);
+}
