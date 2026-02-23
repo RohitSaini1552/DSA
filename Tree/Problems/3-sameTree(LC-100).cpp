30944310f8dc0cc1c5904fd070b7ec317d3f63a7
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-02-23 22:42:41 +0530

    add code to check if 2 given binary trees are same or not

---
+++ Tree/Problems/3-sameTree(LC-100).cpp
@@ -0,0 +1,17 @@
+//Leetcode-100
+/*Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.*/
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+        if(p->val != q->val) return false;
+        bool leftAns = isSameTree(p->left, q->left);
+        if(leftAns == false) return false;
+        bool rightAns = isSameTree(p->right, q->right);
+        if(rightAns == false) return false;
+        return true;
+        
+    }
+};
