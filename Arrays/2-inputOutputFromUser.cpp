49a03d6131a47765099e3a70106fad03181f2961
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-03-07 17:58:38 +0530

    add code to take input and print output of an array using while loop

--- Arrays/2-inputOutputFromUser.cpp
+++ Arrays/2-inputOutputFromUser.cpp
@@ -5,12 +5,24 @@ int main(){
     cout<<"Enter the size of the array : ";
     cin>>n;
     int a[n];
-    cout<<"Enter the elements of array : ";
+    /*
+     cout<<"Enter the elements of array : ";
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
     cout<<"The elements of array are : ";
     for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
+    } 
+        */
+    int i = 0;
+    while(i < n){
+        cin>> a[i];
+        i++;
+    }
+    int j = 0;
+    while(j < n){
+        cout<< a[j]<<" ";
+        j++;
     }
 }
