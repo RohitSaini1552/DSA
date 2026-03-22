71c370ce3c20edad168b5c7902b8b0aafcb33343
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-03-22 23:04:53 +0530

    added code to perform searching(find) operation in a set

---
+++ Sets/3-searchingInSets.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int main(){
+    unordered_set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(5);
+    s.insert(4);
+    s.insert(3);
+    cout<<s.size()<<endl;
+    int target = 40;
+    if(s.find(target) != s.end()) cout<< target <<" found in set";
+    if(s.find(target) == s.end()) cout<< target <<" not found in set";
+
+}
