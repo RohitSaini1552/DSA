86ccbda81d1c0a97177031342c78e399b89f41af
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-03-22 23:03:54 +0530

    added code to erase a element from a set and display set elements by iterating through its elements

---
+++ Sets/2-deletionAndDisplayInSets.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int main(){
+    unordered_set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(5);
+    s.insert(4);
+    s.insert(3);
+    cout<<s.size()<<endl;
+    for(int ele : s){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+    s.erase(5);
+    cout<<s.size()<<endl;
+    for(int ele : s){
+        cout<<ele<<" ";
+    }
+}
