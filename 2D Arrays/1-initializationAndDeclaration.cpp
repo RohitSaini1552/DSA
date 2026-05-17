cf8daba29e78796e06ec2225f29acebc386a94af
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-05-17 23:48:17 +0530

    add code to declare and initialize a 2D array

---
+++ 2D Arrays/1-initializationAndDeclaration.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[2][3] = {{1,2,3},{4,5,6}};
+    int brr[2][3] = {1,2,3,4,5,6};
+    int crr[][3] = {1,2,3,4,5,6};
+    cout<<arr[1][1]<<endl;
+    cout<<brr[1][1]<<endl;
+    cout<<crr[1][1];
+
+}
