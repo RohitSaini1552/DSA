dab5678c8c3f842149ba9f1bae2d72a55e69b5c7
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-05-17 23:48:50 +0530

    add code to traverse a 2D array

---
+++ 2D Arrays/2-traversalInMatrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter rows and columns in matrix :";
+    int r, c;
+    cin>>r>>c;
+    int a[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<"The entered Matrix is :"<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
