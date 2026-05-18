61444514d428c7e49fc45b89101bb5523447c950
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-05-18 22:35:13 +0530

    add code to find the transpose of a matrix

---
+++ 2D Arrays/6-transposeOfMatrix.cpp
@@ -0,0 +1,29 @@
+// write a code to find transpose of a matrix
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[2][3] = {1,2,3,4,5,6};
+    // M1 : only printing of transpose
+    /* for(int j=0;j<3;j++){
+        for(int i=0;i<2;i++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    } */
+
+    // M2 : storing in a new matrix
+   /* int T[3][2];
+   for(int i=0;i<3;i++){
+    for(int j=0;j<2;j++){
+        T[i][j] = arr[j][i];
+    }
+   }
+   for(int i=0;i<3;i++){
+    for(int j=0;j<2;j++){
+        cout<<T[i][j]<<" ";
+    }
+    cout<<endl; 
+   }*/
+
+  
+}
