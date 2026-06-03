79cecf2fde814205199324b6bfe0afc38503cfef
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-06-03 16:24:17 +0530

    add code to calculate and print maze path using 2 parameters(D-down and R-right)

--- Recursion/14_mazePath.cpp
+++ Recursion/14_mazePath.cpp
@@ -1,15 +1,20 @@
+/* Write a code to calculate total possible paths to reach to destination cell starting from 1,1 position
+also prints the path of steps taken
+ */
+
 
 #include<iostream>
 using namespace std;
-void printPath(int er,int ec,int sr,int sc,string p){
-    if(sr>er || sc>ec) return ;
-    if(sr==er && sc==ec){
-        cout<<p<<endl;
+int mazePath(int er,int ec,string s){//we can pass the same string witht the refrence too but then we have to use backtracking to explore other choices in the path too
+    if(er < 1 || ec < 1) return 0;
+    if(er == 1 && ec == 1){
+        cout<<s<<endl; // int this method each recursive path to destination shares and modifies the same string
+         return 1;
     }
-    printPath(er,ec,sr,sc+1,p+'R');
-    printPath(er,ec,sr+1,sc,p+'D');
+    return mazePath(er - 1,ec,s+'R') + mazePath(er,ec-1,s+'D');
 
 }
 int main(){
-    printPath(2,2,0,0,"");
+    string s = "";
+    cout<<mazePath(2,2,s)<<endl;
 }
