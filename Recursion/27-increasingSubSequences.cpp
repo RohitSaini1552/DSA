62ca96a38f958fa0c7775eb7eec1c94f3eac1e0e
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-06-09 23:48:38 +0530

    added code for the increasing subsequences from the elements of a given array

---
+++ Recursion/27-increasingSubSequences.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void subsets(int arr[],int n,int idx,vector<int>ans,int k){
+    if(idx == n){
+        if(ans.size() == k){
+            for(int i=0;i<ans.size();i++)
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    if(ans.size() + (n - idx) < k) return;
+    subsets(arr,n,idx+1,ans,k);
+    ans.push_back(arr[idx]);
+    subsets(arr,n,idx+1,ans,k);
+}
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> v;
+    int k = 3;
+    subsets(arr,n,0,v,k);
+}
