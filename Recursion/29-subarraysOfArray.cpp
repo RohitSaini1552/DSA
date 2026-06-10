ea9e90bb128065abe407dd9cf13925f08414b275
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-06-10 16:43:05 +0530

    added code to print subarrays of an array using iterative method

--- Recursion/29-subarraysOfArray.cpp
+++ Recursion/29-subarraysOfArray.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void subarrays(vector<int> ans,int arr[],int i,int n){
+/* void subarrays(vector<int> ans,int arr[],int i,int n){
     if(i == n){
         for(int i=0;i<ans.size();i++){
             cout<<ans[i]<<" ";
@@ -19,4 +19,18 @@ int main(){
     int n = sizeof(arr)/sizeof(arr[0]);
     vector<int> ans;
     subarrays(ans,arr,0,n);
+} */
+
+
+int main(){
+    int arr[] = {1,2,3,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            for(int k=i;k<=j;k++){
+                cout<<arr[k]<<" ";
+            }
+            cout<<endl;
+        }
+    }
 }
