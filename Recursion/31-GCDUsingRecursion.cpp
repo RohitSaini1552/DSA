a5208178712903146d61b4adf8e80b60469e1656
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-06-11 14:46:30 +0530

    added code to find the GCD using euclid division lemma and recursion

--- Recursion/31-GCDUsingRecursion.cpp
+++ Recursion/31-GCDUsingRecursion.cpp
@@ -1,11 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// M1 : Iterative approach
 int gcd(int a,int b){
     for(int i=min(a,b);i>=2;i--){
         if(a % i == 0 && b % i == 0) return i;
     }
     return 1;
 }
+
+// M2 : Recursive approach
+int gcd(int a,int b,int i){
+    // if(i < 2) return 1;
+    if(a % i == 0 && b % i == 0) return i;
+    return gcd(a,b,i-1);
+}
+ // M3 : Using euclid division lemma
+ int hcf(int a,int b){
+    if(a == b) return a;
+    if(a > b){
+        if(a % b == 0) return b;
+        else return(b,a % b);
+    }
+    else{
+        if(b % a == 0) return a;
+        else return(a,b % a);
+    }
+    return 1;
+ }
+
 int main(){
-    cout<<gcd(24,60)<<endl;
+    cout<<gcd(24,60,24)<<endl;
+    cout<<hcf(24,60)<<endl;
 }
