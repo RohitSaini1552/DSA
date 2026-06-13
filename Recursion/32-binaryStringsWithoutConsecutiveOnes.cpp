95f94ad02ca5d9ff5b67027dc855347a8223d8db
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-06-13 20:04:02 +0530

    added code for build up recursive function to generate all binary strings without consecutive 1s

--- Recursion/32-binaryStringsWithoutConsecutiveOnes.cpp
+++ Recursion/32-binaryStringsWithoutConsecutiveOnes.cpp
@@ -3,6 +3,8 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
+// count down recursion Without extra parameter
 void generateStrings(int n,string ans){
     if(n == 0){
         cout<<ans<<endl;
@@ -11,8 +13,20 @@ void generateStrings(int n,string ans){
     if((ans.size() == 0 || ans.back() == '0')) generateStrings(n-1,ans+'1');
     generateStrings(n-1,ans+'0');
 }
+// build up recursion Without extra parameter
+void generate(int &n,string ans){
+    if(ans.size() == n){
+        cout<<ans<<endl;
+        return;
+    }
+    if((ans.size() == 0 || ans.back() == '0')) generate(n,ans+'1'); // can use ans[ans.length()-1] instead of ans.back()
+    generate(n,ans+'0');
+}
+
 int main(){
     int n;
     cin>>n;
     generateStrings(n,"");
+    generate(n,"");
+    
 }
