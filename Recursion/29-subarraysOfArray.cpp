896c982d53f5ffcf80dcbd90eaa72a7c36d95adf
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-06-17 23:53:43 +0530

    added code for finding all different subarrays of an array using recursion

--- Recursion/29-subarraysOfArray.cpp
+++ Recursion/29-subarraysOfArray.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-/* void subarrays(vector<int> ans,int arr[],int i,int n){
+void subarrays(vector<int> ans,int arr[],int i,int n){
     if(i == n){
         for(int i=0;i<ans.size();i++){
             cout<<ans[i]<<" ";
@@ -8,8 +8,8 @@ using namespace std;
         cout<<endl;
         return;
     }
-    subarrays(ans,arr,i+1,n);
-    if(ans.size() == 0 || ans.back() == arr[i-1]){
+    subarrays(ans,arr,i+1,n); // exlusive calls always without any condition
+    if(ans.size() == 0 || ans.back() == arr[i-1]){ // inclusive calls only when either te ans vecor is empty or it is continous
         ans.push_back(arr[i]);
         subarrays(ans,arr,i+1,n);
     }
@@ -19,10 +19,10 @@ int main(){
     int n = sizeof(arr)/sizeof(arr[0]);
     vector<int> ans;
     subarrays(ans,arr,0,n);
-} */
+}
 
-
-int main(){
+// M2 : using iterative approach
+/* int main(){
     int arr[] = {1,2,3,4};
     int n = sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n;i++){
@@ -33,4 +33,4 @@ int main(){
             cout<<endl;
         }
     }
-}
+} */
