674b213bb4bfe7180e35a5be6ef0ec86f0f51e22
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-15 21:38:13 +0530

    addec code for leetcode 151 reverse words ina string using vector and reverse of a vector

--- Strings/Problems/5-reverseWordsInString(LC-151).cpp
+++ Strings/Problems/5-reverseWordsInString(LC-151).cpp
@@ -1,4 +1,4 @@
-// M1
+// M1 : using string building logic using pointers and separated by space
 
 class Solution {
 public:
@@ -20,6 +20,34 @@ public:
                 
             } 
             j--;
+        }
+        return ans;
+    }
+};
+
+
+// M2 : make all the strings individually and push into a vector or string and then reverse that string or vector and return
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        vector<string> words;
+        int i = 0;
+        string temp = "";
+        while(i < n){
+            if(s[i] != ' ') temp += s[i];
+            if(temp != "" && s[i] == ' '){
+                words.push_back(temp);
+                temp.clear();
+            }
+            i++;
+        }
+        if(temp != "") words.push_back(temp);
+        reverse(words.begin(), words.end());
+        string ans = "";
+        for(int i=0;i<words.size();i++){
+            ans += words[i];
+            if(i != words.size()-1) ans += ' ';
         }
         return ans;
     }
