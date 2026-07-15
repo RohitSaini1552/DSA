13e3dfe8811e0959c74366dcd817bfd2d7c6896c
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-15 21:39:11 +0530

    added code for the LC - 151 using string building and stack for reverse words logic

--- Strings/Problems/5-reverseWordsInString(LC-151).cpp
+++ Strings/Problems/5-reverseWordsInString(LC-151).cpp
@@ -52,3 +52,36 @@ public:
         return ans;
     }
 };
+
+
+
+// M3 : using string building(within the given IP string separated by " ") and stack for reverse logic
+
+class Solution {
+public:
+    string reverseWords(string s) {
+       int n = s.size();
+       int i = 0;
+       string str = "";
+       stack<string> st;
+       while(i < n){
+            if(str.size() == 0 && s[i] == ' ') continue;
+            else if(s[i] != ' ') str += s[i];
+            else if(str.size() != 0 && s[i] == ' '){
+                st.push(str);
+                str.clear();
+            }
+            i++;
+       }
+       if(str != "") st.push(str);
+       string ans = "";
+       while(!st.empty()){
+            string t = st.top();
+            st.pop();
+            ans += t;
+            ans += " ";
+       }
+       ans.pop_back();
+       return ans;
+    }
+};
