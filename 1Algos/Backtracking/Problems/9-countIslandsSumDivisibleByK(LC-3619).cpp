19d7ac47d83f376682b7c2613435d5357e7118b8
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-18 00:33:46 +0530

    added code for LC 3619 by writing base case inside the recursive function and using demorgans law

--- 1Algos/Backtracking/Problems/9-countIslandsSumDivisibleByK(LC-3619).cpp
+++ 1Algos/Backtracking/Problems/9-countIslandsSumDivisibleByK(LC-3619).cpp
@@ -32,3 +32,34 @@ public:
     }
 };
 
+// M2 : Writing base case inside the DFS code
+class Solution {
+public:
+    int dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+    void dfs(int &m,int &n,int r,int c,vector<vector<int>> &grid,long long &sum){
+        if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] <= 0) return;
+        sum += grid[r][c];
+        grid[r][c] = 0;
+        for(auto &x : dir){
+            int nr = r + x[0];
+            int nc = c + x[1];
+            dfs(m,n,nr,nc,grid,sum);
+        }
+        return;
+    }
+    int countIslands(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] > 0){
+                    long long sum = 0;
+                    dfs(m,n,i,j,grid,sum);
+                    if(sum % k == 0) count++;
+                }
+            }
+        }
+        return count;
+    }
+};
