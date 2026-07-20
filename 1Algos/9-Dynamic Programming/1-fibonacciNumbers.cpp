74a8410d75be61fcd989f4b18c5eb8636c1721fc
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-20 23:15:53 +0530

    added code for fibonacci numbers using recursion with memoization

--- 1Algos/9-Dynamic Programming/1-fibonacciNumbers.cpp
+++ 1Algos/9-Dynamic Programming/1-fibonacciNumbers.cpp
@@ -1,11 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<int> dp;
 int fibo(int n){
     if(n == 0 || n == 1) return n;
-    return fibo(n-1) + fibo(n-2);
+    if(dp[n] != -1) return dp[n];
+    return dp[n] = fibo(n-1) + fibo(n-2);
 }
 
 int main(){
     int n = 5;
+    dp.resize(n + 1,-1);
     cout<<fibo(5);
 }
