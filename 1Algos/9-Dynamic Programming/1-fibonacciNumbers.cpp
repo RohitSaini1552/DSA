c1056b15b2131ea99c87d13fd8d35e859aa932a1
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-20 23:53:17 +0530

    added code for calculating fibonacci numbers using Tabulation method

--- 1Algos/9-Dynamic Programming/1-fibonacciNumbers.cpp
+++ 1Algos/9-Dynamic Programming/1-fibonacciNumbers.cpp
@@ -1,14 +1,23 @@
+// Write a code to print Nth fibonacci number using DP
+
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> dp;
-int fibo(int n){
+int fibo1(int n, vector<int> &dp1){
     if(n == 0 || n == 1) return n;
-    if(dp[n] != -1) return dp[n];
-    return dp[n] = fibo(n-1) + fibo(n-2);
+    if(dp1[n] != -1) return dp1[n];
+    return dp1[n] = fibo1(n-1, dp1) + fibo1(n-2, dp1);
 }
 
 int main(){
     int n = 5;
-    dp.resize(n + 1,-1);
-    cout<<fibo(5);
+    vector<int> dp1(n+1,-1);
+    if(n == 0 || n == 1) return n;
+    vector<int> dp2(n+1,-1);
+    dp2[0] = 0;
+    dp2[1] = 1;
+    for(int i=2;i<=n;i++){
+        dp2[i] = dp2[i-1] + dp2[i-2];
+    }
+    cout<<dp2[n]<<endl;
+    cout<<fibo1(5,dp1);
 }
