e5830314cb4b1767a3c82fa290256e0d0f47b24d
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-22 14:45:55 +0530

    added code for remove digits using tabulation Dp

--- 1Algos/9-Dynamic Programming/5-removeDigits.cpp
+++ 1Algos/9-Dynamic Programming/5-removeDigits.cpp
@@ -10,18 +10,36 @@ vector<int> getDigits(int n){
     }
     return v;
 }
-int f(int n){
+int f(int n, vector<int> &dp){
     if(n == 0) return 0;
     if(n > 0 && n <= 9) return 1;
+    if(dp[n] != -1) return dp[n];
     vector<int> v = getDigits(n);
     int ans = INT_MAX;
     for(auto &x : v){
-        ans = min(ans,f(n - x));
+        ans = min(ans,f(n - x,dp));
     }
-    return 1 + ans;
+    return dp[n] = 1 + ans;
+}
+int tabulation(int n, vector<int> &dp2){
+    for(int i=0;i<=n;i++){
+        if(i == 0) dp2[i] = 0;
+        else if(i > 0 && i <= 9) dp2[i] = 1;
+        else{
+            vector<int> v = getDigits(i);
+            int ans = INT_MAX;
+            for(auto &x : v){
+                ans = min(ans,dp2[i-x]);
+            }
+            dp2[i] = 1 + ans;
+        }
+    }
+    return dp2[n];
 }
 int main(){
     int n;
     cin>>n;
-    cout<<f(n);
+    vector<int> dp(n+1,-1);
+    vector<int> dp2(n+1,-1);
+    cout<<f(n,dp)<<" "<<tabulation(n,dp2);
 }
