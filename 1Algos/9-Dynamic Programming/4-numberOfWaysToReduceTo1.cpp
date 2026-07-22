61cd24679985787ec7b123f8925f6a23d895ede8
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-22 13:16:12 +0530

    added code for min number of ways to reduce number to 1 using tabulation

--- 1Algos/9-Dynamic Programming/4-numberOfWaysToReduceTo1.cpp
+++ 1Algos/9-Dynamic Programming/4-numberOfWaysToReduceTo1.cpp
@@ -11,16 +11,22 @@ using namespace std;
 int f(int n, vector<int> &dp){
     if(n == 1) return 0;
     if(dp[n] != -1) return dp[n];
-    int ans1 = f(n-1,dp);
-    int ans2 = INT_MAX;
-    int ans3 = INT_MAX;
-    if(n % 2 == 0) ans2 = f(n/2,dp);
-    if(n % 3 == 0) ans3 = f(n/3,dp);
-    return dp[n] = 1 + min({ans1,ans2,ans3});
+    return dp[n] = 1 + min({f(n-1,dp),(n % 2 == 0) ? f(n/2,dp) : (n % 3 == 0) ? f(n/3,dp) : INT_MAX});
 }
 int main(){
     int n;
     cin>>n;
+    if(n <= 1){
+        cout<<0;
+        return 0;
+    }
     vector<int> dp(n+1,-1);
-    cout<<f(n,dp);
+    vector<int> dp2(n+1,-1);
+    dp[1] = 0;
+    dp[2] = dp[3] = 1;
+    for(int i=3;i<=n;i++){
+        dp[i] = 1 + min({dp[i-1],(i % 2 == 0) ? dp[i/2] : (i % 3 == 0) ? dp[i/3] :INT_MAX});
+    }
+    cout<<f(n,dp)<<endl;
+    cout<<dp[n];
 }
