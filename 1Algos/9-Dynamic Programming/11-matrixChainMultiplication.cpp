7480590b7ea78b0d8cf63d1737fdc0b1ca05ec19
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-27 16:36:51 +0530

    added code for matrix chain multiplication using bottom up dp

--- 1Algos/9-Dynamic Programming/11-matrixChainMultiplication.cpp
+++ 1Algos/9-Dynamic Programming/11-matrixChainMultiplication.cpp
@@ -19,5 +19,16 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>v[i];
     }
+    vector<vector<int>> dp2(n,vector<int>(n,0));
+    for(int len = 3;len <= n;len++){
+        for(int i = 0;i + len - 1 < n;i++){
+            int j = len + i - 1;
+            dp2[i][j] = INT_MAX;
+            for(int k = i+1 ;k < j; k++){
+                dp2[i][j] = min(dp2[i][j],dp2[i][k] + dp2[k][j] + v[i]*v[j]*v[k]);
+            }
+        }
+    }
+    cout<<dp2[0][n-1]<<endl;
     cout<<f(0,n-1,v);
 }
