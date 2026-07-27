78c2c765ec8555e9770ac3148174e4de100b27c6
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-07-27 16:18:24 +0530

    added code for matrix chain multiplication using recurison and memoization

--- 1Algos/9-Dynamic Programming/11-matrixChainMultiplication.cpp
+++ 1Algos/9-Dynamic Programming/11-matrixChainMultiplication.cpp
@@ -1,18 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+vector<vector<int>> dp;
 int f(int i, int j, vector<int> &v){
     if(i == j || i + 1 == j) return 0; // only q matrix available and its cost  = 0
+    if(dp[i][j] != -1) return dp[i][j];
     int ans = INT_MAX;
     for(int k = i+1;k < j;k++){
         ans = min(ans,(f(i,k,v) + f(k,j,v) + v[i] * v[j] * v[k]));
     }
-    return ans;
+    return dp[i][j] = ans;
 }
 
 int main(){
     int n;
     cin>>n;
+    dp.resize(n,vector<int>(n,-1));
     vector<int> v(n);
     for(int i=0;i<n;i++){
         cin>>v[i];
