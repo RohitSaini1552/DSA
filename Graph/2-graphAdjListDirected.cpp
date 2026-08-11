03789bfbe7ea8378b68c0e11cae3bc2b838c04d1
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-08-12 01:53:54 +0530

    add code for implemeting directed graph using adjaceny list

---
+++ Graph/2-graphAdjListDirected.cpp
@@ -0,0 +1,35 @@
+// Write code for implementing directed graph using adj list
+
+
+#include<bits/stdc++.h>
+using namespace std;
+vector<list<int>> graph;
+void addEdge(int src, int dest, bool biDir = true){
+    graph[src].push_back(dest);
+    if(biDir){
+        graph[dest].push_back(src);
+    }
+}
+
+void display(){
+    for(int i=0;i < graph.size();i++){
+        cout<<i<<"->";
+        for(auto &x : graph[i]){
+            cout<<x<<",";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){  
+    int v;
+    int e;
+    cin>>v>>e;
+    graph.resize(v,list<int> ());
+    while(e--){
+        int src, dest;
+        cin>>src>>dest;
+        addEdge(src,dest,false); // Now order of nodes of directed edge matters(from src to dest)
+    }
+    display();
+}
